bce41830897eefa6a661123287196b35c2e6ba25
Shakib Ansari <shakibansari7406@gmail.com>
2025-06-29 23:40:25 +0530

    feat: add CORS support for API to allow cross-origin requests

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -15,6 +16,13 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://funkystitch.in'],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
